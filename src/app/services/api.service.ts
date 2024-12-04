6dd381d77e5e0c12254d6c85a35b74975252cb39
Tony <tony603t@gmail.com>
2024-12-05 00:15:20 +0530

    cookpedia

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  server_url = "http://localhost:3000"
+
+  constructor(private http:HttpClient) { }
+
+  getAllRecipeAPI(){
+    return this.http.get(`${this.server_url}/all-recipes`)
+  }
+
+  // add-testimony
+  addTestimonyAPI(reqBody:any){
+    return this.http.post(`${this.server_url}/add-testimony`,reqBody)
+  }
+}
